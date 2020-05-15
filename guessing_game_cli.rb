9225286790ab59e6f87f72c1778dc993f90bb71c
Matt Guttman <msguttman@gmail.com>
2020-05-15 12:05:06 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,15 @@
 # Code your solution here!
+
+
+
+
+
+
+- Generate and store a random number between 1 and 6
+- Prompts the user to guess their own number between 1 and 6
+- Capture user input from the command line
+- Compare that input to the random number that has been generated
+- Print out one of three statements:
+  - If the user's input matches the random number: `You guessed the correct number!`
+  - If the user's input DOES NOT matches the random number: `Sorry! The computer guessed <number>.`
+  - It the user's input is equal to "exit": `Goodbye!`
