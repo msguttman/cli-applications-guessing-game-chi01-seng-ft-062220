6871b5a2ae6c5efdaeea7d500cfa6a3d39c9b991
Matt Guttman <msguttman@gmail.com>
2020-05-15 12:38:34 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -12,8 +12,17 @@ def prompt_user
   guess
 end
 
+def compare_numbers(rand_int, user_guess)
+  if rand_int == user_guess
+    puts "You guessed the correct number!"
+  elsif user_guess == "exit"
+    puts "Goodbye!"
+  else
+    "Sorry! The computer guessed #{rand_int}."
+  end
+end
 
-
+def 
 - Generate and store a random number between 1 and 6
 - Prompts the user to guess their own number between 1 and 6
 - Capture user input from the command line
